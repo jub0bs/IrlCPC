894c9e053cdedefcc412d688db7582b159353260
Julien Cretel <j.cretel@umail.ucc.ie>
2015-04-03 22:45:06 +0100

    Improve Haskell solution for 2015, pb 2 (Ethel's Encryption)

--- 2015/p2-encryption/encryption.hs
+++ 2015/p2-encryption/encryption.hs
@@ -1,18 +1,18 @@
-import Control.Applicative
-import Data.Char
+import Control.Applicative  ((<$>))
+import Data.Char            (chr, isSpace, ord)
 
-main = do (n:a:b:[]) <- map read . words <$> getLine :: IO [Integer]
-          xs         <- getLine
+main = do (n : a : b : []) <- map read . words <$> getLine :: IO [Integer]
+          xs               <- getLine
           putStrLn $ shiftStr (offset a b) xs
 
--- offset a b : the result of 'a' to the power 'b' modulo 26
+-- offset a b : the opposite of 'a' to the power 'b', modulo 26
 offset :: Integer -> Integer -> Int
 offset a b = negate $ fromIntegral $ (a' ^ b) `mod` 26
   where
     a' = a `mod` 26
 
--- shifChar o c : the character obtained by shifting character 'c' by 'n' places
---                (a space character is unaffected)
+-- shiftChar o c : the character obtained by shifting character 'c'
+--                 by 'n' places (a space character is unaffected)
 shiftChar :: Int -> Char -> Char
 shiftChar o c
   | isSpace c   = c
