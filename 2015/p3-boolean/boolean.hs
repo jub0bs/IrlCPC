8e5e03d7c530fb556a34aa9104dcdd548095eff0
Julien Cretel <j.cretel@umail.ucc.ie>
2015-05-14 16:46:28 +0100

    Improve Haskell solution for 2015, pb 3 (Boolean Postfix)

--- 2015/p3-boolean/boolean.hs
+++ 2015/p3-boolean/boolean.hs
@@ -1,29 +1,24 @@
-import Control.Applicative
+import Control.Applicative ((<$>))
+import Control.Monad       (replicateM_)
 
+main :: IO ()
 main = do n <- read <$> getLine :: IO Int
-          calcMany n
+          replicateM_ n calcOne
 
 -- calcOne : process one line of input
 calcOne :: IO ()
-calcOne = do
-    (_ : ts) <- words <$> getLine
+calcOne =
+    words <$> getLine >>= \ (_ : ts) ->
     if eval' ts []
     then print 1
     else print 0
-
--- calcMany n : process 'n' lines of input
-calcMany :: Int -> IO ()
-calcMany 1 = calcOne
-calcMany n = do
-    calcOne
-    calcMany (n - 1)
 
 -- eval src : the result of the evaluation of source 'src'
 eval :: String -> Bool
 eval src = eval' (words src) []
 
 -- eval src bs : the result of the evaluation of source 'src'
---               using stack 'stack'
+--               using Boolean stack 'bs'
 eval' :: [String] -> [Bool] -> Bool
 eval' []     [b] = b
 eval' (t:ts) bs
@@ -34,18 +29,22 @@ eval' (t:ts) bs
     | t == "X" = eval' ts (applyXor bs)
     | t == "N" = eval' ts (applyNot bs)
 
--- applyAnd bs : the stack obtained by applying an AND operator on stack 'bs'
+-- applyAnd bs : the stack obtained by applying an AND operator on the top
+--               two elements of Boolean stack 'bs'
 applyAnd :: [Bool] -> [Bool]
 applyAnd (b1 : b2 : bs) = (b1 && b2) : bs
 
--- applyOr bs : the stack obtained by applying an OR operator on stack 'bs'
+-- applyOr bs : the stack obtained by applying an OR operator on the top two
+--              elements of Boolean stack 'bs'
 applyOr :: [Bool] -> [Bool]
 applyOr  (b1 : b2 : bs) = (b1 || b2) : bs
 
--- applyXor bs : the stack obtained by applying an XOR operator on stack 'bs'
+-- applyXor bs : the stack obtained by applying an XOR operator on the top
+--               two elements of Boolean stack 'bs'
 applyXor :: [Bool] -> [Bool]
 applyXor (b1 : b2 : bs) = (b1 /= b2) : bs
 
--- applyNot bs : the stack obtained by applying a NOT operator on stack 'bs'
+-- applyNot bs : the stack obtained by applying a NOT operator on the top
+--               element of Boolean stack 'bs'
 applyNot :: [Bool] -> [Bool]
 applyNot (b : bs) = not b : bs
