7cffa8d7396547a1fbad387c7554e46ab0474463
Julien Cretel <j.cretel@umail.ucc.ie>
2015-05-15 03:56:50 +0100

    Improve Haskell solution for 2014, pb 5 (Perfect Pancake Preparation)

--- 2014/p5-pancakes/pancakes.hs
+++ 2014/p5-pancakes/pancakes.hs
@@ -1,45 +1,36 @@
-import Control.Applicative ((<$>))
-import Data.List           (sort)
+import            Control.Applicative         ((<$>))
+import            Control.Monad               (replicateM)
+import qualified  Data.ByteString.Char8 as  B (getLine, readInt, words)
+import            Data.List                   (sort)
+import            Data.Maybe                  (fromJust)
 
+main :: IO ()
 main = do
     n  <- read <$> getLine :: IO Int
-    ps <- getPoints n
+    ps <- replicateM n getPoint
     let ps' = convexHull ps
     print $ length ps'
-    printPoints ps'
+    mapM_ printPoint ps'
 
 getPoint :: IO Point
 getPoint = do
-    [x, y] <- map read . words <$> getLine
+    [x, y] <- map (fst . fromJust . B.readInt) . B.words <$> B.getLine
     return $ Point x y
 
-getPoints :: Int -> IO [Point]
-getPoints 0 = return []
-getPoints n = do
-    p  <- getPoint
-    ps <- getPoints (n - 1)
-    return (p : ps)
+printPoint :: Point -> IO ()
+printPoint (Point x y) = putStrLn $ show x ++ " " ++ show y
 
-printPoints :: [Point] -> IO ()
-printPoints []            = return ()
-printPoints (Point x y : ps) = do
-    putStrLn $ show x ++ " " ++ show y
-    printPoints ps
-
-------------------------------
--- Monotone-chain algorithm --
-------------------------------
-
-data Point  = Point !Int !Int
-    deriving (Eq, Ord)
-data Vector = Vector !Int !Int
+----- Monotone-chain algorithm -----
+type Scalar = Int
+data Point  = Point  !Scalar !Scalar deriving (Eq, Ord)
+data Vector = Vector !Scalar !Scalar
 
 -- vec p1 p2 : the vector between the two points 'p1' and 'p2'
 vec :: Point -> Point -> Vector
 vec (Point x1 y1) (Point x2 y2) = Vector (x2 - x1) (y2 - y1)
 
--- cross v1 v2 : the cross-product of vectors 'v1' and 'v2'
-cross :: Vector -> Vector -> Int
+-- cross v1 v2 : the cross product of vectors 'v1' and 'v2'
+cross :: Vector -> Vector -> Scalar
 cross (Vector x1 y1) (Vector x2 y2) = x1 * y2 - y1 * x2
 
 -- isClockwise o a b : is angle (OA, OB) a clockwise turn?
@@ -52,6 +43,7 @@ convexHull :: [Point] -> [Point]
 convexHull []     = []
 convexHull [p]    = [p]
 convexHull points = lower ++ upper
+-- convexHull points = last upper : lower ++ init upper
   where
     sorted = sort points
     lower  = chain sorted
@@ -62,9 +54,8 @@ convexHull points = lower ++ upper
 chain :: [Point] -> [Point]
 chain = go []
   where
-    go acc@(r1 : r2 : rs) (x : xs) =
-        if isClockwise r2 r1 x
-        then go (r2 : rs) (x : xs)
-        else go (x : acc) xs
+    go acc@(r1 : r2 : rs) (x : xs) = if isClockwise r2 r1 x
+                                     then go (r2 : rs) (x : xs)
+                                     else go (x : acc) xs
     go acc                (x : xs) = go (x : acc) xs
     go acc                []       = reverse $ tail acc
