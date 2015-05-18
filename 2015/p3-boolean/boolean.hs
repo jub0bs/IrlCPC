68ed8b82b4d4ba756cff5cb5029ddc3d8021e0d9
Julien Cretel <j.cretel@umail.ucc.ie>
2015-05-18 16:20:58 +0100

    Improve Haskell solution for 2015, pb 3 (Boolean Postfix)

--- 2015/p3-boolean/boolean.hs
+++ 2015/p3-boolean/boolean.hs
@@ -13,6 +13,12 @@ calcOne =
     then print 1
     else print 0
 
+-- Op : a Boolean operator
+data Op = And
+        | Or
+        | Xor
+        | Not
+
 -- eval src : the result of the evaluation of source 'src'
 eval :: String -> Bool
 eval src = eval' (words src) []
@@ -20,31 +26,22 @@ eval src = eval' (words src) []
 -- eval src bs : the result of the evaluation of source 'src'
 --               using Boolean stack 'bs'
 eval' :: [String] -> [Bool] -> Bool
-eval' []     [b] = b
-eval' (t:ts) bs
+eval' []       [b] = b
+eval' (t : ts) bs
     | t == "0" = eval' ts (False : bs)
     | t == "1" = eval' ts (True  : bs)
-    | t == "A" = eval' ts (applyAnd bs)
-    | t == "R" = eval' ts (applyOr  bs)
-    | t == "X" = eval' ts (applyXor bs)
-    | t == "N" = eval' ts (applyNot bs)
+    | t == "A" = eval' ts (apply And bs)
+    | t == "R" = eval' ts (apply Or  bs)
+    | t == "X" = eval' ts (apply Xor bs)
+    | t == "N" = eval' ts (apply Not bs)
+    | otherwise = error "eval' : invalid token"
+eval' _        _ = error "eval' : invalid arguments"
 
--- applyAnd bs : the stack obtained by applying an AND operator on the top
---               two elements of Boolean stack 'bs'
-applyAnd :: [Bool] -> [Bool]
-applyAnd (b1 : b2 : bs) = (b1 && b2) : bs
-
--- applyOr bs : the stack obtained by applying an OR operator on the top two
---              elements of Boolean stack 'bs'
-applyOr :: [Bool] -> [Bool]
-applyOr  (b1 : b2 : bs) = (b1 || b2) : bs
-
--- applyXor bs : the stack obtained by applying an XOR operator on the top
---               two elements of Boolean stack 'bs'
-applyXor :: [Bool] -> [Bool]
-applyXor (b1 : b2 : bs) = (b1 /= b2) : bs
-
--- applyNot bs : the stack obtained by applying a NOT operator on the top
---               element of Boolean stack 'bs'
-applyNot :: [Bool] -> [Bool]
-applyNot (b : bs) = not b : bs
+-- apply op bs : the Boolean stack obtained by applying Boolean operator 'op'
+--               on the Boolean stack 'bs'
+apply :: Op -> [Bool] -> [Bool]
+apply And (b1 : b2 : bs) = (b1 && b2) : bs
+apply Or  (b1 : b2 : bs) = (b1 || b2) : bs
+apply Xor (b1 : b2 : bs) = (b1 /= b2) : bs
+apply Not (b : bs)       = not b : bs
+apply _   _              = error "apply : invalid argument(s)"
